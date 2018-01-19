93d1a4c03f27f0f32daaf74fbe413f62eb907579
pattrickrice <patrickrice20@gmail.com>
2018-01-19 13:19:26 -0800

    add parsing of JSON object into list of data model

--- app/src/main/java/com/patrick/guidebookcodechallenge/UpcomingGuides.java
+++ app/src/main/java/com/patrick/guidebookcodechallenge/UpcomingGuides.java
@@ -9,6 +9,8 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -59,7 +61,55 @@ public class UpcomingGuides extends AppCompatActivity {
 
                     Log.v(LOGTAG, jsondata);
 
-                    
+                    // Handle jsonException
+
+                    try {
+                        jsonObject = new JSONObject(jsondata);
+                        JSONArray jsonArray = jsonObject.getJSONArray(Keys.KEY_DATA);
+
+                        int lengthOfJSONArray = jsonArray.length();
+                        if (lengthOfJSONArray > 0) {
+                            for (int i = 0; i < lengthOfJSONArray; i++) {
+                                GuideDataModel model = new GuideDataModel();
+
+                                // parse JSON object
+                                JSONObject guideItem = jsonArray.getJSONObject(i);
+                                String startDate = guideItem.getString(Keys.KEY_START_DATE);
+                                String endDate = guideItem.getString(Keys.KEY_END_DATE);
+                                String url = guideItem.getString(Keys.KEY_URL);
+                                String name = guideItem.getString(Keys.KEY_NAME);
+                                String icon = guideItem.getString(Keys.KEY_ICON);
+
+
+                                // set to data structure
+                                model.setStartDate(startDate);
+                                model.setEndDate(endDate);
+                                model.setUrl(url);
+                                model.setName(name);
+                                model.setIcon(icon);
+
+                                // getting info from venue object
+                                JSONObject venueItem = guideItem.getJSONObject(Keys.KEY_VENUE);
+
+                                // URL did not return item in venue object at time of writing this.
+                                if (venueItem.has(Keys.KEY_CITY) && venueItem.has(Keys.KEY_STATE)) {
+                                    String city = guideItem.getString(Keys.KEY_CITY);
+                                    String state = guideItem.getString(Keys.KEY_STATE);
+
+                                    // set to model
+                                    model.setCity(city);
+                                    model.setState(state);
+                                }
+
+                                // add to the list
+                                // MILESTONE 2: Parse the data retrieved from the server into a list of Java objects
+                                upcomingGuides.add(model);
+                            }
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
