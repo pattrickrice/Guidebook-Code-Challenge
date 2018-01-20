51a1ba7e37288ee2dcf17d2b1c71def3333fa408
pattrickrice <patrickrice20@gmail.com>
2018-01-20 13:32:53 -0800

    Polish project

--- app/src/main/java/com/patrick/guidebookcodechallenge/UpcomingGuideAdapter.java
+++ app/src/main/java/com/patrick/guidebookcodechallenge/UpcomingGuideAdapter.java
@@ -2,6 +2,7 @@ package com.patrick.guidebookcodechallenge;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +18,11 @@ import java.util.List;
 /**
  * RecyclerView Adapter for upcoming guides list
  */
-
 public class UpcomingGuideAdapter extends ArrayAdapter<GuideDataModel> {
     private List<GuideDataModel> guideModelList;
     private Context context;
     private LayoutInflater mInflator;
 
-    // Required constructor
     UpcomingGuideAdapter(Context context, List<GuideDataModel> guideObjects) {
         super(context, 0, guideObjects);
         this.context = context;
@@ -50,6 +49,9 @@ public class UpcomingGuideAdapter extends ArrayAdapter<GuideDataModel> {
 
         GuideDataModel guideModel = getGuideItem(position);
 
+        // MILESTONE 4: In addition the object’s name, have your view display
+        // the image located at each object’s icon url.
+
         // load image
         Picasso.with(context).load(guideModel
                 .getIcon())
@@ -60,14 +62,31 @@ public class UpcomingGuideAdapter extends ArrayAdapter<GuideDataModel> {
         viewHolder.nameView.setText(guideModel.getName());
         viewHolder.cityView.setText(guideModel.getCity());
         viewHolder.stateView.setText(guideModel.getState());
-        viewHolder.startDateView.setText(String.format("%s - ", guideModel.getStartDate()));
-        viewHolder.endDateView.setText(guideModel.getEndDate());
+        viewHolder.startDateView.setText(guideModel.getStartDate());
+        viewHolder.endDateView.setText(String.format(" - %s", guideModel.getStartDate()));
+
+        // Handle if city and state objects are not present
+        if (guideModel.getState() == "" && guideModel.getCity() == "") {
+            viewHolder.cityStateLayout.setVisibility(View.GONE);
+        } else if (guideModel.getCity() == "") {
+            viewHolder.cityView.setVisibility(View.INVISIBLE);
+        } else if (guideModel.getState() == "") {
+            viewHolder.stateView.setVisibility(View.INVISIBLE);
+        } else {
+            viewHolder.cityStateLayout.setVisibility(View.VISIBLE);
+            viewHolder.cityView.setVisibility(View.VISIBLE);
+            viewHolder.stateView.setVisibility(View.VISIBLE);
+        }
 
         return viewHolder.rootView;
     }
 
+    /**
+     * Viewholder contains handles to all of the views
+     */
     static class ViewHolder {
         final RelativeLayout rootView;
+        final LinearLayoutCompat cityStateLayout;
         final ImageView iconView;
         final TextView nameView;
         final TextView startDateView;
@@ -76,14 +95,16 @@ public class UpcomingGuideAdapter extends ArrayAdapter<GuideDataModel> {
         final TextView cityView;
 
         private ViewHolder(RelativeLayout rootView,
+                           LinearLayoutCompat cityStateLayout,
                            ImageView iconView,
                            TextView nameView,
                            TextView startDateView,
                            TextView endDateView,
                            TextView stateView,
                            TextView cityView) {
-            this.iconView = iconView;
             this.rootView = rootView;
+            this.cityStateLayout = cityStateLayout;
+            this.iconView = iconView;
             this.nameView = nameView;
             this.startDateView = startDateView;
             this.endDateView = endDateView;
@@ -92,6 +113,7 @@ public class UpcomingGuideAdapter extends ArrayAdapter<GuideDataModel> {
         }
 
         private static ViewHolder create(RelativeLayout rootView) {
+            LinearLayoutCompat cityStateLayout = rootView.findViewById(R.id.city_and_state_layout);
             ImageView iconView = rootView.findViewById(R.id.guide_icon_view);
             TextView nameView = rootView.findViewById(R.id.guide_name_tv);
             TextView startDateView = rootView.findViewById(R.id.start_date_tv);
@@ -99,7 +121,7 @@ public class UpcomingGuideAdapter extends ArrayAdapter<GuideDataModel> {
             TextView stateView = rootView.findViewById(R.id.state_tv);
             TextView cityView = rootView.findViewById(R.id.city_tv);
 
-            return new ViewHolder(rootView, iconView, nameView, startDateView, endDateView, stateView, cityView);
+            return new ViewHolder(rootView, cityStateLayout, iconView, nameView, startDateView, endDateView, stateView, cityView);
         }
     }
 }
